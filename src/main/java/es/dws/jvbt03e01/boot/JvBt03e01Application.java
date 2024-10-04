4505631b8274c53d84ad3c6b3f43ca2c50b011d7
Jose Vasquez <joseevasquezbarboza@gmail.com>
2024-10-04 17:54:08 +0200

    inital commit

---
+++ src/main/java/es/dws/jvbt03e01/boot/JvBt03e01Application.java
@@ -0,0 +1,13 @@
+package es.dws.jvbt03e01.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvBt03e01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvBt03e01Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/dws/jvbt03e01/JvBt03e01ApplicationTests.java
@@ -0,0 +1,15 @@
+package es.dws.jvbt03e01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import es.dws.jvbt03e01.boot.JvBt03e01Application;
+
+@SpringBootTest(classes = JvBt03e01Application.class)
+class JvBt03e01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
